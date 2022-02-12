95cd4065984354585ddc8b22f6173d958bfd9384
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2022-02-12 23:44:47 +0530

    gestureState global context

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,19 @@
 import { SketchComp2 } from "./sketch/SketchComp2";
 import "./App.css";
 import Video from "./streamer/Video";
+import GestureState from "./context/GestureState";
 
 function App() {
-  const sketch = (p5) => {
-    p5.setup = () => p5.createCanvas(600, 400, p5.WEBGL);
-
-    p5.draw = () => {
-      p5.background(250);
-      p5.normalMaterial();
-      p5.push();
-      p5.rotateZ(p5.frameCount * 0.01);
-      p5.rotateX(p5.frameCount * 0.01);
-      p5.rotateY(p5.frameCount * 0.01);
-      p5.plane(100);
-      p5.pop();
-    };
-  };
-
   return (
-    <div className="App">
-      <SketchComp2></SketchComp2>
-      <h1 className="heading">Finguitar</h1>
-      <div className="cam"><Video></Video></div>
-    </div>
+    <GestureState>
+      <div className="App">
+        <SketchComp2></SketchComp2>
+        <h1 className="heading">Finguitar</h1>
+        <div className="cam">
+          <Video></Video>
+        </div>
+      </div>
+    </GestureState>
   );
 }
 export default App;
