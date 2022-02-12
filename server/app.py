2cdbfb6a990436656be38e11deb6dd5bae844a2f
Jash-Shah <jash28582@gmail.com>
2022-02-12 20:01:13 +0530

    Base64 decoding on backend done

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, jsonify
 from flask_sock import Sock
 from flask_socketio import SocketIO
@@ -6,6 +7,9 @@ import cv2 as cv
 import mediapipe as mp
 from landmark_processing import *
 import base64
+from PIL import Image
+from io import BytesIO
+import numpy as np
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -23,14 +27,14 @@ hands = mp_hands.Hands(
 )
 
 
-@app.route("/keypoint_classifier/")
-def hello_world():
+def get_sign(image):
     # get image by converting from base64
     image = cv.flip(image, 1)
     # Detection implementation #############################################################
     image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
     image.flags.writeable = False
     results = hands.process(image)
+    print("Result = ",results.multi_hand_landmarks)
     image.flags.writeable = True
     if results.multi_hand_landmarks is not None:
         for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
@@ -41,19 +45,20 @@ def hello_world():
                 landmark_list)
             # Hand sign classification
             hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
-
-    return jsonify(hand_sign_id)
-
+    return hand_sign_id
 
 @sock.route('/')
 def echo(ws):
+    i = 0
     while True:
         data = ws.receive()
-        data_bytes = base64.b64decode(data)
-        # result = data_bytes.decode('windows-1252')
-        # print(result)
+        data_bytes = base64.b64decode(data[23:])
+        if len(data_bytes) != 0:
+            img_result = Image.open(BytesIO(data_bytes))
+            print(get_sign(np.asarray(img_result)))
+            img_result.save(f"image.jpeg","JPEG")
         # print(type(data_bytes))
-        ws.send(data)
+        ws.send("Received!")
 
 
 if __name__ == "__main__":
