523f760a3c3fa8fa68a2afc6430ee4b43a75a51a
Pratiksha Sankhe <pssankhe_b20@ce.vjti.ac.in>
2022-02-12 12:57:30 +0530

    Basic pg

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
+import NET from "vanta/dist/vanta.net.min";
+import * as THREE from "three";
 
 function App() {
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const vantaRef = useRef(null);
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        NET({
+          el: vantaRef.current,
+          THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          points: 19.0,
+          maxDistance: 19.0,
+          spacing: 12.0,
+          color:0x215896,
+          backgroundColor:0x7192f,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destory();
+    };
+  }, [vantaEffect]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{ height: "100vh", width: "100%", overflow: "hidden" }}
+        ref={vantaRef}
+      >
+        <h1 className="heading">Finguitar</h1>
+        <div className="cam"></div>
+      </div>
     </div>
   );
 }
-
 export default App;
